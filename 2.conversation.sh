f5db0f0587710626b663c24ccda064f49fc55785
Nagarjuna <drnagarjuna23@gmail.com>
2025-10-30 23:30:06 +0530

    some message

--- 2.conversation.sh
+++ 2.conversation.sh
@@ -2,5 +2,5 @@
 
 Person1=sachin
 Person2=rahul
-echo "Person1:: hi Person2, how are you?"
-echo "Person2:: hello Person1, i am fine"
+echo "$Person1:: hi $Person2, how are you?"
+echo "$Person2:: hello $Person1, i am fine"
